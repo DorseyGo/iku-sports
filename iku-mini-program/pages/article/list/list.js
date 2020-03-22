5b7816b35f933d6e6c02901f897a41fa192a2c41
tangqifeng1111@163.com <tangqifeng1111@163.com>
2020-03-22 20:35:06 +0800

    add article

---
+++ iku-mini-program/pages/article/list/list.js
@@ -0,0 +1,45 @@
+// pages/article/list/list.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
