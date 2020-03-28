f464c0b3b9499088505bfe00e1df8fc54541ef36
feizhaobao <51563619+feizhaobao@users.noreply.github.com>
2020-03-28 15:10:13 +0800

    Sumbit class modules

---
+++ iku-mini-program-service/src/main/java/com/iku/sports/mini/admin/entity/Klass.java
@@ -0,0 +1,20 @@
+package com.iku.sports.mini.admin.entity;
+
+import java.math.BigInteger;
+
+/**
+ * File: Klass
+ * Author: Huanghz
+ * Created: 2020/3/28
+ * Description:
+ * CopyRight: All rights reserved
+ **/
+public class Klass {
+   private Integer id;
+   private String title;
+   private Integer chapter;
+   private String content;
+   private BigInteger watches;
+   private Integer courseId;
+   private Integer teacherId;
+}
