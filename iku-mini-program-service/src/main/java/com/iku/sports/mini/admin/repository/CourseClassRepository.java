bec226c7a77617aed3d6f0d05a31259d26d3b726
feizhaobao <51563619+feizhaobao@users.noreply.github.com>
2020-03-29 15:27:51 +0800

    change courseclassrespository

--- iku-mini-program-service/src/main/java/com/iku/sports/mini/admin/repository/CourseClassRepository.java
+++ iku-mini-program-service/src/main/java/com/iku/sports/mini/admin/repository/CourseClassRepository.java
@@ -1,11 +1,9 @@
 package com.iku.sports.mini.admin.repository;
 
-import ch.qos.logback.core.joran.conditional.ThenAction;
 import com.iku.sports.mini.admin.entity.CourseClass;
 import org.apache.ibatis.annotations.*;
 import org.apache.ibatis.jdbc.SQL;
 import org.apache.ibatis.type.JdbcType;
-import org.checkerframework.checker.units.qual.C;
 import org.springframework.stereotype.Repository;
 
 import java.util.Arrays;
@@ -36,64 +34,60 @@ public interface CourseClassRepository {
     })
 
     @ResultMap("courseClassRM")
-    @SelectProvider(type = CourseSqlProvider.class,method = "getFirst3ClassesByCourseId")
+    @SelectProvider(type = CourseClassSqlProvider.class,method = "getFirst3ClassesByCourseId")
     List<CourseClass> getFirst3ClassesByCourseId(@Param("first3ClassCourseId") short courseId);
 
     @ResultMap("courseClassRM")
-    @SelectProvider(type = CourseSqlProvider.class,method = "paginateClasses")
+    @SelectProvider(type = CourseClassSqlProvider.class,method = "paginateClasses")
     List<CourseClass> paginateClasses(@Param("paginateCourseId") short courseId,
                                       @Param("paginateOffset") int offset,
                                       @Param("paginatePageSize") int pageSize);
     @ResultMap("courseClassRM")
-    @SelectProvider(type = CourseSqlProvider.class,method = "getClassById")
+    @SelectProvider(type = CourseClassSqlProvider.class,method = "getClassById")
     CourseClass getClassById(@Param("calssId") int id);
 
     @ResultMap("courseClassRM")
-    @SelectProvider(type = CourseSqlProvider.class,method = "getTop3PopularClasses")
+    @SelectProvider(type = CourseClassSqlProvider.class,method = "getTop3PopularClasses")
     List<CourseClass> getTop3PopularClasses(@Param("popularId") short categoryId);
 
     @ResultMap("courseClassRM")
-    @SelectProvider(type = CourseSqlProvider.class,method = "getTop3ClassicByCategoryId")
+    @SelectProvider(type = CourseClassSqlProvider.class,method = "getTop3ClassicByCategoryId")
     List<CourseClass> getTop3ClassicByCategoryId(@Param("top3CategoryId") short categoryId,
                                                  @Param("top3CategoryDays") int days);
 
-    class CourseSqlProvider{
+    class CourseClassSqlProvider {
         static final List<String> ALLCOLS = Arrays.asList("id","title","cover","chapter","video_url","content","watches","course_id","teacher_id");
         static final List<String> SIMPLECOLS = Arrays.asList("t.id","t.title","t.cover","t.content","t.watches","t.course_id");
 
-        public String getTop3ClassicByCategoryId(final Map<String,Object> param){
+        public String getTop3ClassicByCategoryId(final Map<String,Object> params){
             return new SQL(){
                 {
                     SELECT(SIMPLECOLS.toArray(new String[SIMPLECOLS.size()]));
                     FROM(CLASSTABLE+" T");
                     LEFT_OUTER_JOIN(COURSETABLE+" T2 ON T.course_id = T2.id");
-                    if("#{top3CategoryId}" != null){
+                    if(params.get("top3CategoryId") != null){
                         WHERE("T2.category_id=#{top3CategoryId}");
                     }
-                    if("#{top3CategoryDays}" != null) {
+
+                    if(params.get("top3CategoryDays") != null) {
                         WHERE("t.create_time < date_substr(sysdate(),interval #{days} day)");
-                    }else{
-                        WHERE("1=2");
                     }
                 }
             }.toString();
         }
 
-        public String getTop3PopularClasses(final Map<String,Object> param){
+        public String getTop3PopularClasses(final Map<String,Object> params){
             return new SQL(){
                 {
                     SELECT(SIMPLECOLS.toArray(new String[SIMPLECOLS.size()]));
                     FROM(CLASSTABLE+" T");
                     LEFT_OUTER_JOIN(COURSETABLE+" T2 ON T.course_id = T2.id");
-                    if ("#{popularId}" != null){
+                    if (params.get("popularId") != null){
                         WHERE("T2.category_id=#{popularId}");
                     }
-                    if (("#{paginateOffset}" != null) && ("#{paginatePageSize}") != null ){
+                    if ((params.get("paginateOffset") != null) && (params.get("paginatePageSize")) != null ){
                         ORDER_BY("T.create_time desc,T.watches desc limit #{paginateOffset},#{paginatePageSize}");
-                    }else{
-                        ORDER_BY("T.create_time desc,T.watches desc");
                     }
-
                 }
             }.toString();
         }
@@ -102,30 +96,25 @@ public interface CourseClassRepository {
                 {
                     SELECT(ALLCOLS.toArray(new String[ALLCOLS.size()]));
                     FROM(CLASSTABLE+" T");
-                    if ("#{classId}" != null){
+                    if (param.get("classId") != null){
                         WHERE(" T.ID = #{classId}");
-                    }else{
-                        WHERE("1=2");
                     }
                     ORDER_BY("T.create_time desc,T.watches desc");
                 }
             }.toString();
         }
 
-        public String paginateClasses(final Map<String,Object> param) {
+        public String paginateClasses(final Map<String,Object> params) {
             return new SQL(){
                 {
                     SELECT(SIMPLECOLS.toArray(new String[SIMPLECOLS.size()]));
                     FROM(CLASSTABLE+" T");
-                    if ("#{paginateCourseId}" != null){
+                    if (params.get("paginateCourseId") != null){
                         WHERE("course_id=#{paginateCourseId}");
                     }
-                    if (("#{paginateOffset}" != null) && ("#{paginatePageSize}") != null ){
+                    if ((params.get("paginateOffset") != null) && (params.get("paginatePageSize")) != null ){
                         ORDER_BY("T.create_time desc,T.watches desc limit #{paginateOffset},#{paginatePageSize}");
-                    }else{
-                        ORDER_BY("T.create_time desc,T.watches desc");
                     }
-
                 }
             }.toString();
         }
@@ -136,10 +125,8 @@ public interface CourseClassRepository {
                     SELECT(SIMPLECOLS.toArray(new String[SIMPLECOLS.size()]));
                     FROM(CLASSTABLE+" T");
                     LEFT_OUTER_JOIN(COURSETABLE+" T2 ON T.course_id = T2.ID");
-                    if ("#{first3ClassCourseId}" != null) {
+                    if (param.get("first3ClassCourseId") != null) {
                         WHERE("T2.ID = #{first3ClassCourseId}");
-                    }else {
-                        WHERE("1=2");
                     }
                     ORDER_BY("T.course_id DESC LIMIT 0,3");
 
