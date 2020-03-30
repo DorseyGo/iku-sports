080874016cb4b06edc1d2358f38529ac41381043
DorseyGo <tangqifeng1111@163.com>
2020-03-30 10:39:10 +0800

    modify the course class service implementation

--- iku-mini-program-service/src/main/java/com/iku/sports/mini/admin/service/impl/CourseClassServiceImpl.java
+++ iku-mini-program-service/src/main/java/com/iku/sports/mini/admin/service/impl/CourseClassServiceImpl.java
@@ -1,8 +1,10 @@
 package com.iku.sports.mini.admin.service.impl;
 
+import com.google.common.collect.Lists;
 import com.iku.sports.mini.admin.entity.CourseClass;
 import com.iku.sports.mini.admin.repository.CourseClassRepository;
 import com.iku.sports.mini.admin.service.CourseClassService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -17,10 +19,13 @@ import java.util.List;
  * Description:Implements CourseClassService
  * CopyRight: All rights reserved
  **/
+@Slf4j
 @Service("courseClassService")
 public class CourseClassServiceImpl implements CourseClassService {
     private final CourseClassRepository courseClassRepository;
-    private final int pageSizeArrays[]={10,20,50,100};
+    private final List<Integer> ALLOWED_PAGE_SIZES = Lists.newArrayList(10, 20, 50, 100);
+    static final int DEFAULT_PAGE_SIZE = 10;
+
     @Autowired
     public CourseClassServiceImpl(@Qualifier("courseClassRepository") CourseClassRepository courseClassRepository) {
         this.courseClassRepository = courseClassRepository;
@@ -28,31 +33,34 @@ public class CourseClassServiceImpl implements CourseClassService {
 
 
     @Override
-    public List<CourseClass> getFirst3ClassesByCourseId(short courseId) throws Exception{
+    public List<CourseClass> getFirst3ClassesByCourseId(short courseId) throws Exception {
         return courseClassRepository.getFirst3ClassesByCourseId(courseId);
     }
 
     @Override
-    public List<CourseClass> paginateClasses(short courseId, int offset, int pageSize) throws Exception{
-        int isExists = Arrays.binarySearch(pageSizeArrays,pageSize);
-        if (isExists <= 0 ){
-            pageSize = 20;
+    public List<CourseClass> paginateClasses(short courseId, int offset, int pageSize) throws Exception {
+        if (!ALLOWED_PAGE_SIZES.contains(pageSize)) {
+            log.info("Page size fallback, due to passed-in page size: {}, not contained in: {}",
+                    pageSize, ALLOWED_PAGE_SIZES);
+
+            pageSize = DEFAULT_PAGE_SIZE;
         }
-        return courseClassRepository.paginateClasses(courseId,offset,pageSize);
+
+        return courseClassRepository.paginateClasses(courseId, offset, pageSize);
     }
 
     @Override
-    public CourseClass getClassById(int id) throws Exception{
+    public CourseClass getClassById(int id) throws Exception {
         return courseClassRepository.getClassById(id);
     }
 
     @Override
-    public List<CourseClass> getTop3PopularClasses(short categoryId) throws Exception{
-        return courseClassRepository.getTop3PopularClasses(categoryId) ;
+    public List<CourseClass> getTop3PopularClasses(short categoryId) throws Exception {
+        return courseClassRepository.getTop3PopularClasses(categoryId);
     }
 
     @Override
-    public List<CourseClass> getTop3ClassicByCategoryId(short categoryId, int days) throws Exception{
-        return courseClassRepository.getTop3ClassicByCategoryId(categoryId,days);
+    public List<CourseClass> getTop3ClassicByCategoryId(short categoryId, int days) throws Exception {
+        return courseClassRepository.getTop3ClassicByCategoryId(categoryId, days);
     }
 }
