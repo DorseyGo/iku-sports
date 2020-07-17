5e10d57ebaf41fee7f3f67352874130c21b98383
DorseyGo <tangqifeng1111@163.com>
2020-07-17 16:52:38 +0800

    modify about

--- iku-mini-program/pages/about/about.js
+++ iku-mini-program/pages/about/about.js
@@ -1,102 +1,49 @@
 // pages/about/about.js
 const request  = require("../../utils/request");
 
+const menus = [ {
+    "avatar": "../../images/list.png",
+    "name": "我的订单",
+    "forwardTo": "../order/order",
+    "disable": false
+  }, {
+    "avatar": "../../images/calendar.png",
+    "name": "课程预约",
+    "forwardTo": "../appoint/appoint",
+    "disable": false
+  }, {
+    "avatar": "../../images/post.png",
+    "name": "帖子管理",
+    "disable": true
+  }, {
+    "avatar": "../../images/feedback.png",
+    "name": "反馈建议",
+    "disable": true
+  }, {
+    "avatar": "../../images/settings.png",
+    "name": "设置",
+    "disable": true
+  }, {
+    "avatar": "../../images/more.png",
+    "name": "更多",
+    "disable": true
+  }
+]
+
 Page({
 
   /**
    * Page initial data
    */
   data: {
-    userInfo: {},
-    showLearned: true,
-    classes: [],
-    user: {},
-    userId: "e9b6ea6f672086252a83a48be2198d63",
-    list:[{
-      url:"../../images/header.jpeg",
-      name:"初级网球课"
-    },{
-      url:"../../images/header.jpeg",
-      name:"初级网球课"
-    },
-    {
-      url:"../../images/header.jpeg",
-      name:"初级网球课"
-    },
-    {
-      url:"../../images/header.jpeg",
-      name:"初级网球课"
-    }]
-  },
-
-  toggleLearned: function() {
-    let showLearned = !this.data.showLearned
-    this.setData({
-      showLearned: showLearned
-    })
-
-    this.loadClasses()
-  },
-
-  loadClasses: function() {
-    let token = this.data.userId
-    let reqUrl = (this.data.showLearned) ? `watched/classes` : `favorite/classes`
-    request.get(`${reqUrl}`, {
-      token: token
-    }).then(res => {
-      this.setData({
-        classes: res.data
-      })
-    })
-  },
-
-  loadUser: function() {
-    let token = this.data.userId
-    request.get(`users/${token}`).then(res => {
-      this.setData({
-        user: res.data
-      })
-    })
+    user: null,
+    menus: menus
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-    // this.loadUser()
-    wx.login({
-      success (res) {
-        console.log(res.code)
-        if (res.code) {
-          //发起网络请求
-          wx.request({
-            url: 'http://localhost:9080/sports/user/login',
-            data: {
-              code: res.code
-            }
-          })
-        } else {
-          console.log('登录失败！' + res.errMsg)
-        }
-      }
-    })
-    this.loadClasses();
-  },
-
-  handleGetUserInfo: function (e) {
-    console.log(e);
-    console.log(e.detail.userInfo.avatarUrl)
-    wx.setStorageSync('userinfo', e.detail.userInfo);
-    this.setData({
-      userInfo: e.detail.userInfo,
-    })
-  },
-
-  onShow: function(options) {
-    const userinfo = wx.getStorageSync('userinfo')
-    this.setData({
-      userInfo: userinfo,
-      hasUserInfo: true
-    })
   }
+
 })
